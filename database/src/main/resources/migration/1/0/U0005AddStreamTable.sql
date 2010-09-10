d229cf1d9b0681ef8b220078e030f7e75b55e64c
Blake Caldwell <w.blake.caldwell@lmco.com>
2010-09-10 15:08:25 -0400

    Wired Streams to People and creation the migration script.

---
+++ database/src/main/resources/migration/1/0/U0005AddStreamTable.sql
@@ -0,0 +1,30 @@
+--
+-- Set database version to 1.0.0005
+--
+
+insert into db_version (major, minor, patch, scriptname, description) values (1, 0, '0005', 'U0005AddStreamTable.sql', 'Add Stream table.');
+
+
+create table Stream (
+    id  bigserial not null,
+    version int8 not null,
+    name varchar(255) not null,
+    readOnly bool not null,
+    request varchar(255) not null,
+    primary key (id)
+);
+
+
+create table Person_Stream (
+    Person_id int8 not null,
+    streams_id int8 not null,
+    streamIndex int4 not null,
+    primary key (Person_id, streamIndex)
+);
+
+
+alter table Person_Stream
+    add constraint FKBEF553AA3C9AB417
+    foreign key (Person_id)
+    references Person;
+
