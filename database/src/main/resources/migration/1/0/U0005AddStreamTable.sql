b2bcd6abd6e795e5c910ac4696270a3d74d59f8c
Rob Keane <robert.c.keane@lmco.com>
2010-09-13 15:08:49 -0400

    Fixes incorrectly merged DB script

--- database/src/main/resources/migration/1/0/U0005AddStreamTable.sql
+++ database/src/main/resources/migration/1/0/U0005AddStreamTable.sql
@@ -1,19 +1,6 @@
 --
 -- Set database version to 1.0.0005
 --
-
-<<<<<<< HEAD
-insert into db_version (major, minor, patch, scriptname, description) values (1, 0, '000x5', 'U0004AddStreamTable.sql', 'Add stream table.');
-
-    create table Stream (
-        id  bigserial not null,
-        version int8 not null,
-        name varchar(255) not null,
-        readOnly bool not null,
-        request text not null,
-        primary key (id)
-    );
-=======
 insert into db_version (major, minor, patch, scriptname, description) values (1, 0, '0005', 'U0005AddStreamTable.sql', 'Add Stream table.');
 
 
@@ -39,5 +26,3 @@ alter table Person_Stream
     add constraint FKBEF553AA3C9AB417
     foreign key (Person_id)
     references Person;
->>>>>>> 21e03a7e77f5aee1feba813a18bada5826598348
-
