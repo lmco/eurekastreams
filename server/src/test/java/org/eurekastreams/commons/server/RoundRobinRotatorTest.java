15c2792018ab192c34203447d959fe081665dbbc
cdscott <chad.d.scott@lmco.com>
2010-09-30 12:13:38 -0400

    Incremental cache warm refactor check-in. Added generic Rotator interface and generic RoundRobinRotator implementation.

---
+++ server/src/test/java/org/eurekastreams/commons/server/RoundRobinRotatorTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2009-2010 Lockheed Martin Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eurekastreams.commons.server;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * Test for RoundRobinRotator.
+ * 
+ */
+public class RoundRobinRotatorTest
+{
+    /**
+     * List of items.
+     */
+    private List<String> items = new ArrayList<String>(Arrays.asList("woot", "it", "works"));
+
+    /**
+     * System under test.
+     */
+    private RoundRobinRotator<String> sut = new RoundRobinRotator<String>(items);
+
+    /**
+     * Test.
+     */
+    @Test
+    public void test()
+    {
+        assertEquals("woot", sut.getNext());
+        assertEquals("it", sut.getNext());
+        assertEquals("works", sut.getNext());
+        assertEquals("woot", sut.getNext());
+        assertEquals("it", sut.getNext());
+        assertEquals("works", sut.getNext());
+        assertEquals("woot", sut.getNext());
+        assertEquals("it", sut.getNext());
+        assertEquals("works", sut.getNext());
+    }
+
+}
