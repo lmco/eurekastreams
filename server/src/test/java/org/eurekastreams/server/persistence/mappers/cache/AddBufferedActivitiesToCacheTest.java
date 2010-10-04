10b4aaacbb67a147718bd894d04a50b5c786d850
jhstephe <jeffrey.h.stephens@lmco.com>
2010-10-04 11:58:20 -0500

    re-enabling and fixing a unit test

--- server/src/test/java/org/eurekastreams/server/persistence/mappers/cache/AddBufferedActivitiesToCacheTest.java
+++ server/src/test/java/org/eurekastreams/server/persistence/mappers/cache/AddBufferedActivitiesToCacheTest.java
@@ -15,12 +15,13 @@
  */
 package org.eurekastreams.server.persistence.mappers.cache;
 
-import static org.junit.Assert.assertTrue;
-
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.eurekastreams.server.domain.stream.ActivityDTO;
 import org.eurekastreams.server.persistence.mappers.DomainMapper;
+import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.jmock.integration.junit4.JUnit4Mockery;
 import org.jmock.lib.legacy.ClassImposteriser;
@@ -28,8 +29,8 @@ import org.junit.Test;
 
 /**
  * Add buffered activities to the cache.
- *
  */
+@SuppressWarnings("unchecked")
 public class AddBufferedActivitiesToCacheTest
 {
     /**
@@ -64,57 +65,55 @@ public class AddBufferedActivitiesToCacheTest
     @Test
     public void testExecute()
     {
-        assertTrue(true);
-        // AddBufferedActivitiesToCache sut = new AddBufferedActivitiesToCache(bulkActivitiesMapper, cache,
-        // getCompositeStreamsByActivity);
-        //
-        // final List<Long> activityIds = null;
-        //
-        // final ActivityDTO activity1 = context.mock(ActivityDTO.class);
-        // final ActivityDTO activity2 = context.mock(ActivityDTO.class, "a2");
-        //
-        // final List<ActivityDTO> activities = new LinkedList<ActivityDTO>();
-        // activities.add(activity1);
-        // activities.add(activity2);
-        //
-        // final List<Long> followerIdsForAct1 = new LinkedList<Long>();
-        // followerIdsForAct1.add(1L);
-        // followerIdsForAct1.add(2L);
-        // followerIdsForAct1.add(3L);
-        //
-        // final List<Long> followerIdsForAct2 = new LinkedList<Long>();
-        // followerIdsForAct2.add(1L);
-        // followerIdsForAct2.add(2L);
-        // followerIdsForAct2.add(4L);
-        //
-        // context.checking(new Expectations()
-        // {
-        // {
-        // allowing(activity1).getId();
-        // will(returnValue(7L));
-        //
-        // allowing(activity2).getId();
-        // will(returnValue(8L));
-        //
-        // oneOf(cache).setListCAS(CacheKeys.BUFFERED_ACTIVITIES, null);
-        // will(returnValue(activityIds));
-        //
-        // oneOf(bulkActivitiesMapper).execute(activityIds);
-        // will(returnValue(activities));
-        //
-        // oneOf(getCompositeStreamsByActivity).getFollowers(activity1);
-        // will(returnValue(followerIdsForAct1));
-        //
-        // oneOf(getCompositeStreamsByActivity).getFollowers(activity2);
-        // will(returnValue(followerIdsForAct2));
-        //
-        // exactly(8).of(cache).addToTopOfList(with(any(String.class)), with(any(ArrayList.class)));
-        // }
-        // });
-        //
-        // sut.execute();
-        //
-        // context.assertIsSatisfied();
-    }
+        AddBufferedActivitiesToCache sut = new AddBufferedActivitiesToCache(bulkActivitiesMapper, cache,
+                getCompositeStreamsByActivity);
 
+        final List<Long> activityIds = null;
+
+        final ActivityDTO activity1 = context.mock(ActivityDTO.class);
+        final ActivityDTO activity2 = context.mock(ActivityDTO.class, "a2");
+
+        final List<ActivityDTO> activities = new LinkedList<ActivityDTO>();
+        activities.add(activity1);
+        activities.add(activity2);
+
+        final List<Long> followerIdsForAct1 = new LinkedList<Long>();
+        followerIdsForAct1.add(1L);
+        followerIdsForAct1.add(2L);
+        followerIdsForAct1.add(3L);
+
+        final List<Long> followerIdsForAct2 = new LinkedList<Long>();
+        followerIdsForAct2.add(1L);
+        followerIdsForAct2.add(2L);
+        followerIdsForAct2.add(4L);
+
+        context.checking(new Expectations()
+        {
+            {
+                allowing(activity1).getId();
+                will(returnValue(7L));
+
+                allowing(activity2).getId();
+                will(returnValue(8L));
+
+                oneOf(cache).setListCAS(CacheKeys.BUFFERED_ACTIVITIES, null);
+                will(returnValue(activityIds));
+
+                oneOf(bulkActivitiesMapper).execute(activityIds);
+                will(returnValue(activities));
+
+                oneOf(getCompositeStreamsByActivity).getFollowers(activity1);
+                will(returnValue(followerIdsForAct1));
+
+                oneOf(getCompositeStreamsByActivity).getFollowers(activity2);
+                will(returnValue(followerIdsForAct2));
+
+                exactly(5).of(cache).addToTopOfList(with(any(String.class)), with(any(ArrayList.class)));
+            }
+        });
+
+        sut.execute();
+
+        context.assertIsSatisfied();
+    }
 }
