44d6eabae798e89fae467fe592b0f17e114dd90f
jhstephe <jeffrey.h.stephens@lmco.com>
2010-10-04 15:39:13 -0500

    adding code and test to refresh all affected org's cached list of activity ids when a group is moved from one org to another; uses new spring-only mapper/action

--- server/src/main/java/org/eurekastreams/server/action/execution/stream/SyncGroupActivityRecipientParentOrganizationExecution.java
+++ server/src/main/java/org/eurekastreams/server/action/execution/stream/SyncGroupActivityRecipientParentOrganizationExecution.java
@@ -16,6 +16,7 @@
 package org.eurekastreams.server.action.execution.stream;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -37,7 +38,7 @@ import org.eurekastreams.server.search.modelview.OrganizationModelView;
 /**
  * Sync the recipientParentOrg for all activities posted to a group in the DB and queue up task to sync a the groups up
  * in cache and search index upon successful DB update.
- *
+ * 
  */
 public class SyncGroupActivityRecipientParentOrganizationExecution implements
         TaskHandlerExecutionStrategy<ActionContext>
@@ -70,7 +71,7 @@ public class SyncGroupActivityRecipientParentOrganizationExecution implements
 
     /**
      * Constructor.
-     *
+     * 
      * @param inSyncActivityRecipientParentOrg
      *            Mapper to update recipient parent org id for all activites for a group.
      * @param inActivityIdMapper
@@ -99,7 +100,7 @@ public class SyncGroupActivityRecipientParentOrganizationExecution implements
     /**
      * Sync the recipientParentOrg for all activities posted to a group in the DB and queue up task to sync a the groups
      * up in cache and search index upon successful DB update.
-     *
+     * 
      * @param inActionContext
      *            {@link ActionContext}.
      * @return null;
@@ -139,16 +140,14 @@ public class SyncGroupActivityRecipientParentOrganizationExecution implements
             orgIds.addAll(getRecursiveOrgParentMapper.execute(org.getEntityId()));
         }
 
-        // TODO: still needed after StreamViews were deleted?
-        // // get compositeStream ids for all affected orgs
-        // List<OrganizationModelView> allOrgs = getOrgByIdMapper.execute(new ArrayList<Long>(orgIds));
-        // for (OrganizationModelView org : allOrgs)
-        // {
-        // inActionContext.getUserActionRequests().add(
-        // new UserActionRequest("deleteCacheKeysAction", null, new HashSet<String>(Arrays
-        // .asList(CacheKeys.ACTIVITIES_BY_COMPOSITE_STREAM + org.getCompositeStreamId()))));
-        //
-        // }
+        // refresh cached list of org's activity for each affected org
+        List<OrganizationModelView> allOrgs = getOrgByIdMapper.execute(new ArrayList<Long>(orgIds));
+        for (OrganizationModelView org : allOrgs)
+        {
+            inActionContext.getUserActionRequests().add(
+                    new UserActionRequest("refreshOrgActivities", null, org.getShortName()));
+
+        }
 
         return null;
     }
