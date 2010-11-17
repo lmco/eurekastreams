393d0be79dd548bb71e193a284dcf25e221b0a6e
Rob Keane <robert.c.keane@lmco.com>
2010-11-17 14:43:53 -0500

    Fix for tracking mouse position in jQuery 1.4

--- web/src/main/java/org/eurekastreams/web/client/ui/pages/master/ApplicationEntryPoint.java
+++ web/src/main/java/org/eurekastreams/web/client/ui/pages/master/ApplicationEntryPoint.java
@@ -504,8 +504,9 @@ public class ApplicationEntryPoint implements EntryPoint
                 eb.@org.eurekastreams.web.client.events.EventBus::notifyObservers(Ljava/lang/Object;)(notificationEvent);
         }
 
-        $wnd.jQuery($doc).ready(function() {
-                $wnd.jQuery().mousemove(function(e) {
+
+        $wnd.jQuery('body').ready(function() {
+                $wnd.jQuery('body').mousemove(function(e) {
                    $wnd.mouseXpos = e.pageX;
                    $wnd.mouseYpos = e.pageY;
                 });
