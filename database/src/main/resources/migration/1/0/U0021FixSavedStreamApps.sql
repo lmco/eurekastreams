fb9f16380768ca0dd7f343ea625fdfca8953e273
Rob Keane <robert.c.keane@lmco.com>
2011-01-25 08:48:52 -0500

    pulling out bad migration fix

--- database/src/main/resources/migration/1/0/U0021FixSavedStreamApps.sql
+++ database/src/main/resources/migration/1/0/U0021FixSavedStreamApps.sql
@@ -1,7 +1,5 @@
 insert into db_version (major, minor, patch, scriptname, description) values (1, 0, '0021', 'U0021FixSavedStreamApps.sql', 'Fix the saved stream apps.');
 
-Update gadget g2 set gadgetuserpref ='{"gadgetTitle" : "' || coalesce(substring(gadgetuserpref from '"gadgetTitle":"([^"]*)"'), 'Gadget') || E'", "streamQuery":"saved/' || (select s.streamindex from gadget g, person_stream s where g.id = g2.id and  g.gadgetuserpref ~* 'saved/' AND s.streamid = cast(substring(g.gadgetuserpref from '"saved/([^"]*)"') as integer))  || '"}' WHERE g2.gadgetuserpref ~* 'saved/' AND cast(substring(g2.gadgetuserpref from '"saved/([^"]*)"') as integer) > 4;
-
 Update gadget set gadgetuserpref = '{"gadgetTitle" : "Following", "streamQuery":"saved/1"}' where gadgetuserpref ~* '"filterId":"1"';
 Update gadget set gadgetuserpref = '{"gadgetTitle" : "Parent Org", "streamQuery":"saved/2"}' where gadgetuserpref ~* '"filterId":"2"';
 Update gadget set gadgetuserpref = '{"gadgetTitle" : "Everyone", "streamQuery":"saved/3"}' where gadgetuserpref ~* '"filterId":"3"';
