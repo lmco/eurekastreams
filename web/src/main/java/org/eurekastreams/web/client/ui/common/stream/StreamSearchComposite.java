e281bfe7e27ddba3a60e009af4bda8654f6895f7
billwolckenlmco <bill.wolcken@lmco.com>
2011-01-28 15:05:49 -0500

    Fix copyright date and permissions for last commit.

--- web/src/main/java/org/eurekastreams/web/client/ui/common/stream/StreamSearchComposite.java
+++ web/src/main/java/org/eurekastreams/web/client/ui/common/stream/StreamSearchComposite.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2010 Lockheed Martin Corporation
+ * Copyright (c) 2009-2011 Lockheed Martin Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -378,7 +378,9 @@ public class StreamSearchComposite extends FlowPanel implements Bindable
      *            Input string, possibly null.
      * @return JSON string representation.
      */
-    private static native String makeJsonString(final String input) /*-{
-                        return input == null ? 'null' : '"' + input.replace(/\\/g,'\\\\').replace(/"/g,'\\"') + '"';
-                     }-*/;
+    private static native String makeJsonString(final String input)
+    /*-{
+         return input == null ? 'null' : '"' + input.replace(/\\/g,'\\\\').replace(/"/g,'\\"') + '"';
+       }-*/;
 }
+
