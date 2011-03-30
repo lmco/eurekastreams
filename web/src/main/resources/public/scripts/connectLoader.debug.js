931e33829ea77691781b65ae79a1269324f253fb
Rob Keane <robert.c.keane@lmco.com>
2011-03-30 10:16:59 -0400

    Eureka Connect Loader

--- web/src/main/resources/public/scripts/connectLoader.debug.js
+++ web/src/main/resources/public/scripts/connectLoader.debug.js
@@ -1,47 +1,214 @@
-/**
- * Get Elements by Class Name.
- */
-var __eurekaConnect__getElementsByClassName=function(j,k,l){getElementsByClassName=document.getElementsByClassName?function(d,a,b){d=(b||document).getElementsByClassName(d);a=a?RegExp("\\b"+a+"\\b","i"):null;b=[];for(var e,c=0,f=d.length;c<f;c+=1)e=d[c],(!a||a.test(e.nodeName))&&b.push(e);return b}:document.evaluate?function(d,a,b){a=a||"*";b=b||document;var e=d.split(" "),c="",f=document.documentElement.namespaceURI==="http://www.w3.org/1999/xhtml"?"http://www.w3.org/1999/xhtml":null;d=[];for(var h,g=0,i=e.length;g< i;g+=1)c+="[contains(concat(' ', @class, ' '), ' "+e[g]+" ')]";try{h=document.evaluate(".//"+a+c,b,f,0,null)}catch(j){h=document.evaluate(".//"+a+c,b,null,0,null)}for(;a=h.iterateNext();)d.push(a);return d}:function(d,a,b){a=a||"*";b=b||document;var e=d.split(" ");d=[];a=a==="*"&&b.all?b.all:b.getElementsByTagName(a);b=[];var c;c=0;for(var f=e.length;c<f;c+=1)d.push(RegExp("(^|\\s)"+e[c]+"(\\s|$)"));f=0;for(var h=a.length;f<h;f+=1){e=a[f];c=!1;for(var g=0,i=d.length;g<i;g+=1)if(c=d[g].test(e.className), !c)break;c&&b.push(e)}return b};return getElementsByClassName(j,k,l)};
+// START Do Not Touch
+(function(){
+
+    var DomReady = window.DomReady = {};
+
+	// Everything that has to do with properly supporting our document ready event. Brought over from the most awesome jQuery. 
+
+    var userAgent = navigator.userAgent.toLowerCase();
+
+    // Figure out what browser is being used
+    var browser = {
+    	version: (userAgent.match( /.+(?:rv|it|ra|ie)[\/: ]([\d.]+)/ ) || [])[1],
+    	safari: /webkit/.test(userAgent),
+    	opera: /opera/.test(userAgent),
+    	msie: (/msie/.test(userAgent)) && (!/opera/.test( userAgent )),
+    	mozilla: (/mozilla/.test(userAgent)) && (!/(compatible|webkit)/.test(userAgent))
+    };    
+
+	var readyBound = false;	
+	var isReady = false;
+	var readyList = [];
+
+	// Handle when the DOM is ready
+	function domReady() {
+		// Make sure that the DOM is not already loaded
+		if(!isReady) {
+			// Remember that the DOM is ready
+			isReady = true;
+        
+	        if(readyList) {
+	            for(var fn = 0; fn < readyList.length; fn++) {
+	                readyList[fn].call(window, []);
+	            }
+            
+	            readyList = [];
+	        }
+		}
+	};
+
+	// From Simon Willison. A safe way to fire onload w/o screwing up everyone else.
+	function addLoadEvent(func) {
+	  var oldonload = window.onload;
+	  if (typeof window.onload != 'function') {
+	    window.onload = func;
+	  } else {
+	    window.onload = function() {
+	      if (oldonload) {
+	        oldonload();
+	      }
+	      func();
+	    }
+	  }
+	};
+
+	// does the heavy work of working through the browsers idiosyncracies (let's call them that) to hook onload.
+	function bindReady() {
+		if(readyBound) {
+		    return;
+	    }
+	
+		readyBound = true;
+
+		// Mozilla, Opera (see further below for it) and webkit nightlies currently support this event
+		if (document.addEventListener && !browser.opera) {
+			// Use the handy event callback
+			document.addEventListener("DOMContentLoaded", domReady, false);
+		}
+
+		// If IE is used and is not in a frame
+		// Continually check to see if the document is ready
+		if (browser.msie && window == top) (function(){
+			if (isReady) return;
+			try {
+				// If IE is used, use the trick by Diego Perini
+				// http://javascript.nwbox.com/IEContentLoaded/
+				document.documentElement.doScroll("left");
+			} catch(error) {
+				setTimeout(arguments.callee, 0);
+				return;
+			}
+			// and execute any waiting functions
+		    domReady();
+		})();
+
+		if(browser.opera) {
+			document.addEventListener( "DOMContentLoaded", function () {
+				if (isReady) return;
+				for (var i = 0; i < document.styleSheets.length; i++)
+					if (document.styleSheets[i].disabled) {
+						setTimeout( arguments.callee, 0 );
+						return;
+					}
+				// and execute any waiting functions
+	            domReady();
+			}, false);
+		}
+
+		if(browser.safari) {
+		    var numStyles;
+			(function(){
+				if (isReady) return;
+				if (document.readyState != "loaded" && document.readyState != "complete") {
+					setTimeout( arguments.callee, 0 );
+					return;
+				}
+				if (numStyles === undefined) {
+	                var links = document.getElementsByTagName("link");
+	                for (var i=0; i < links.length; i++) {
+	                	if(links[i].getAttribute('rel') == 'stylesheet') {
+	                	    numStyles++;
+	                	}
+	                }
+	                var styles = document.getElementsByTagName("style");
+	                numStyles += styles.length;
+				}
+				if (document.styleSheets.length != numStyles) {
+					setTimeout( arguments.callee, 0 );
+					return;
+				}
+			
+				// and execute any waiting functions
+				domReady();
+			})();
+		}
+
+		// A fallback to window.onload, that will always work
+	    addLoadEvent(domReady);
+	};
+
+	// This is the public function that people can use to hook up ready.
+	DomReady.ready = function(fn, args) {
+		// Attach the listeners
+		bindReady();
+    
+		// If the DOM is already ready
+		if (isReady) {
+			// Execute the function immediately
+			fn.call(window, []);
+	    } else {
+			// Add the function to the wait list
+	        readyList.push( function() { return fn.call(window, []); } );
+	    }
+	};
+    
+	bindReady();
+	
+})();
 // END Do Not Touch
 
-/**
- * Base URL for Eureka Instance.
- */
-__eurekaConnect__baseUrl = "http://keaner.dev.smp.isgs.lmco.com";
-
-/**
- * Eurkea Connect Activitation Class
- */
-__eurekaConnect__activitionClass = "eureka-connect-widget";
-
-if (window.postMessage) {
-    window.addEventListener("message", __eurekaConnect__receiveMessage, false);
-}
-else {
-    window.opener = {};
-    window.opener.postMessage = function(msg) {
-        var packet = {};
-        packet.data = msg;
-        __eurekaConnect__receiveMessage(packet);
-    };
-}
-
-function __eurekaConnect__receiveMessage(event) {
-    var payload = eval( "(" + event.data + ")" );
-    document.getElementById('frameId').height = +payload.frameHeight + 2;
-}
-
 function __eurekaConnect__onLoad() {
+    var __eurekaConnect__baseUrl = function() {
+        var myName = /(^|[\/\\])connectLoader.*\.js(\?|$)/;
+        var scripts = document.getElementsByTagName("script");
+        for (var i = 0; i < scripts.length; i++) {
+            var src;
+            if (src = scripts[i].getAttribute("src")) {
+                if (src.match(myName)) {
+                    return src.substring(0, src.indexOf('/',7));
+                }
+            }
+        }
+    }();
+
+    var widgets = function(oElm, strTagName, strAttributeName){
+        var arrElements = (strTagName == "*" && oElm.all)? oElm.all : oElm.getElementsByTagName(strTagName);
+        var arrReturnElements = new Array();
+        var oCurrent;
+        var oAttribute;
+        for(var i=0; i<arrElements.length; i++) {
+            oCurrent = arrElements[i];
+            oAttribute = oCurrent.getAttribute && oCurrent.getAttribute(strAttributeName);
+            if(typeof oAttribute == "string" && oAttribute.length > 0){
+                arrReturnElements.push(oCurrent);
+            }
+        }
+        return arrReturnElements;
+    }(document,"*","eureka:id");
+
+    var __eurekaConnect__receiveMessage = function(event) {
+        var payload = eval( "(" + event.data + ")" );
+        var frame = document.getElementById(payload.frameId);
+        if (frame != null) {
+            frame.height = +payload.frameHeight + 2;
+        }
+        else {
+            console.log("Frame is null: " + payload.frameId);
+        }
+    }
+
+    if (window.postMessage) {
+        window.addEventListener("message", __eurekaConnect__receiveMessage, false);
+    }
+    else {
+        window.opener = {};
+        window.opener.postMessage = function(msg) {
+            var packet = {};
+            packet.data = msg;
+            __eurekaConnect__receiveMessage(packet);
+        };
+    }
+
     var hostUrl = escape(window.location.protocol + "//" + window.location.host);
-	var widgets =  __eurekaConnect__getElementsByClassName(__eurekaConnect__activitionClass);
 
 	for (i in widgets)
 	{
 		var widget = widgets[i];
 		var widgetType = widget.getAttribute('eureka:widget');
-        widget.innerHTML = "<iframe id='frameId' style='overflow: hidden' frameborder='0' width='" + widget.getAttribute('eureka:width')  + "' src='" + __eurekaConnect__baseUrl  + "/widget.html?p=" + hostUrl + "#widget-" + widgetType + "'></iframe>";
+        var frameId =  "__eurekaConnect__Frame-" + widget.getAttribute('eureka:id');
+        widget.innerHTML = "<iframe id='" + frameId + "' style='overflow: hidden' frameborder='0' width='" + widget.getAttribute('eureka:width')  + "' src='" + __eurekaConnect__baseUrl  + "/widget.html?i=" + escape(frameId) + "&p=" + hostUrl + "#widget-" + widgetType + "'></iframe>";
 
 	}
 }
 
-__eurekaConnect__onLoad()
+DomReady.ready(__eurekaConnect__onLoad);
