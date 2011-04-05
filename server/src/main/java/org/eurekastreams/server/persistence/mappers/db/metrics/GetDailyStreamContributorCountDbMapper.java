2ddbe90f90a8835185236839dfcf29015537a0a6
Blake Caldwell <w.blake.caldwell@lmco.com>
2011-04-05 13:22:05 -0400

    Work toward compiling daily usage metrics.  I have to switch gears temporarily, so I'm checking in these mappers without unit testing. They're commented out to avoid lowering code coverage in the meantime. I'll pick back up on these when I'm done this other task

---
+++ server/src/main/java/org/eurekastreams/server/persistence/mappers/db/metrics/GetDailyStreamContributorCountDbMapper.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2010-2011 Lockheed Martin Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eurekastreams.server.persistence.mappers.db.metrics;
+
+import java.util.Date;
+
+import org.eurekastreams.server.persistence.mappers.BaseArgDomainMapper;
+
+/**
+ * DB Mapper to get the stream contributor count of a specific day.
+ */
+public class GetDailyStreamContributorCountDbMapper extends BaseArgDomainMapper<Date, Long>
+{
+    /**
+     * Get the number of stream contributors on a specific day.
+     * 
+     * @param inDay
+     *            the date to look for stats
+     * @return the number of stream contributors on the input day
+     */
+    @Override
+    public Long execute(final Date inDay)
+    {
+        // Query q;
+        // Date startOfDay, endOfDay;
+        // HashSet<Long> peopleIds = new HashSet<Long>();
+        //
+        // startOfDay = DateDayExtractor.getStartOfDay(inDay);
+        // endOfDay = DateDayExtractor.getEndOfDay(inDay);
+        //
+        // q = getEntityManager().createQuery(
+        // "SELECT DISTINCT(actorId) FROM Activity "
+        // + "WHERE actorType=:actorType AND postedTime >= :startDate AND postedTime <= :endDate)")
+        // .setParameter("startDate", startOfDay).setParameter("endDate", endOfDay).setParameter("actorType",
+        // EntityType.PERSON);
+        // peopleIds.addAll(q.getResultList());
+        //
+        // q = getEntityManager().createQuery(
+        // "SELECT DISTINCT(author.id) FROM Comment WHERE timeSent >= :startDate AND timeSent <= :endDate")
+        // .setParameter("startDate", startOfDay).setParameter("endDate", endOfDay);
+        // peopleIds.addAll(q.getResultList());
+        //
+        // return new Long(peopleIds.size());
+        return 0L;
+    }
+}
