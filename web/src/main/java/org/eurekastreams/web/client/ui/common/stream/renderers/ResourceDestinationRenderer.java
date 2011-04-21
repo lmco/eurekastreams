e1c8efe33834c3e69ba05fa2ea6aa7e80ac9a880
Rob Keane <robert.c.keane@lmco.com>
2011-04-21 13:48:15 -0400

    This was fixed twice

--- web/src/main/java/org/eurekastreams/web/client/ui/common/stream/renderers/ResourceDestinationRenderer.java
+++ web/src/main/java/org/eurekastreams/web/client/ui/common/stream/renderers/ResourceDestinationRenderer.java
@@ -59,13 +59,13 @@ public class ResourceDestinationRenderer implements StatefulRenderer
         Panel main = new FlowPanel();
         main.addStyleName(StaticResourceBundle.INSTANCE.coreCss().inlinePanel());
 
-        main.add(new InlineLabel("commented on "));
+        main.add(new InlineLabel("commented on"));
         main.add(new Anchor(resourceTitle, resourceUrl));
         if (resourceTitle != null && !resourceTitle.isEmpty())
         {
             if (siteUrl != null && !siteUrl.isEmpty())
             {
-                main.add(new InlineLabel(" from "));
+                main.add(new InlineLabel("from"));
                 main.add(new Anchor(siteTitle, siteUrl));
             }
             else
