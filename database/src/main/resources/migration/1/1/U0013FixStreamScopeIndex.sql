baf6e522170b8a89f6f2c24901d8e5a20f791e94
Blake Caldwell <w.blake.caldwell@lmco.com>
2011-05-04 13:35:12 -0400

    Fixed new index for StreamScope - was using an exsting name in build step 1.1.13

--- database/src/main/resources/migration/1/1/U0013FixStreamScopeIndex.sql
+++ database/src/main/resources/migration/1/1/U0013FixStreamScopeIndex.sql
@@ -1,7 +1,7 @@
 insert into db_version (major, minor, patch, scriptname, description) values (1, 1, '0013', 
 'U0013FixStreamScopeIndex', 'Fix stream scope index');
 
-CREATE INDEX streamscope_uniquekey_idx
+CREATE INDEX streamscope_scopeType_uniquekey_idx
   ON streamscope
   USING btree
   (scopeType, uniquekey);
