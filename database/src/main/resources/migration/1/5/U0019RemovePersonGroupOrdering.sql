330fa7b407188bbdc337315bac51c7ab77dd8be9
Blake Caldwell <w.blake.caldwell@lmco.com>
2011-05-18 12:09:41 -0400

    Migration because Chad took my db migration version :|

--- database/src/main/resources/migration/1/5/U0019RemovePersonGroupOrdering.sql
+++
@@ -1,4 +0,0 @@
-insert into db_version (major, minor, patch, scriptname, description) values (1, 5, '0019',
-'U0019RemovePersonGroupOrdering', 'Remove ordering of persons groups');
-
-ALTER TABLE GroupFollower DROP COLUMN groupstreamindex;
