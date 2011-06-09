8eeda7097caa91c2b02bb2ed9815b0e9aa76d005
cdscott <chad.d.scott@lmco.com>
2011-06-09 09:21:21 -0400

    Added Db mapper for fetching FeaturedStream DTOs.

---
+++ server/src/test/java/org/eurekastreams/server/persistence/mappers/db/GetFeaturedStreamDTOsDbMapperTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2010 Lockheed Martin Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eurekastreams.server.persistence.mappers.db;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.eurekastreams.server.domain.dto.FeaturedStreamDTO;
+import org.eurekastreams.server.persistence.mappers.MapperTest;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test for GetFeaturedStreamDTOsDbMapper.
+ * 
+ */
+public class GetFeaturedStreamDTOsDbMapperTest extends MapperTest
+{
+    /**
+     * System under test.
+     */
+    private GetFeaturedStreamDTOsDbMapper sut = new GetFeaturedStreamDTOsDbMapper();
+
+    /**
+     * Setup.
+     */
+    @Before
+    public void setup()
+    {
+        sut.setEntityManager(getEntityManager());
+    }
+
+    /**
+     * Test.
+     */
+    @Test
+    public void test()
+    {
+        List<FeaturedStreamDTO> results = sut.execute(null);
+
+        // assert correct number.
+        assertEquals(2, results.size());
+
+        // assert correct order
+        assertEquals(2, results.get(0).getId().longValue());
+
+    }
+}
