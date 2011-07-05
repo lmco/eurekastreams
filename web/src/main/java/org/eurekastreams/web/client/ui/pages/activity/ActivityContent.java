38203346d671a5ad392971054c5044c1c0ff08ee
Rob Keane <robert.c.keane@lmco.com>
2011-07-05 17:29:58 -0400

    Checkstyle fix

--- web/src/main/java/org/eurekastreams/web/client/ui/pages/activity/ActivityContent.java
+++ web/src/main/java/org/eurekastreams/web/client/ui/pages/activity/ActivityContent.java
@@ -76,7 +76,6 @@ import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.History;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.FocusPanel;
