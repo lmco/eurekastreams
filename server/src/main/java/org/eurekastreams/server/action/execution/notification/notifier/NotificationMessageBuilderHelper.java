e3aeaab477a8a5ab3cbd8871dcfb7fce1fa88e4f
billwolckenlmco <bill.wolcken@lmco.com>
2011-07-19 16:37:52 -0400

    Handle markdown in activity content for notification emails (D-06998)

--- server/src/main/java/org/eurekastreams/server/action/execution/notification/notifier/NotificationMessageBuilderHelper.java
+++ server/src/main/java/org/eurekastreams/server/action/execution/notification/notifier/NotificationMessageBuilderHelper.java
@@ -15,6 +15,10 @@
  */
 package org.eurekastreams.server.action.execution.notification.notifier;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.lang.text.StrLookup;
 import org.apache.commons.lang.text.StrSubstitutor;
 import org.apache.velocity.context.Context;
@@ -31,9 +35,30 @@ public class NotificationMessageBuilderHelper
     /** Eureka variable end marker. */
     private static final String VARIABLE_END_MARKER = "%";
 
+    /** Regex for extracting markdown links. */
+    private static final String MARKDOWN_LINK_REGEX = "\\[([^]]+)\\]\\(([^)]+)\\)";
+
+    /** Compiled regex for extracting markdown links. */
+    private static final Pattern MARKDOWN_LINK_PATTERN = Pattern
+            .compile(MARKDOWN_LINK_REGEX, Pattern.CASE_INSENSITIVE);
+
+    /** Base URL to prepend to relative URLs. */
+    private final String baseUrl;
+
+    /**
+     * Constructor.
+     *
+     * @param inBaseUrl
+     *            Base URL to prepend to relative URLs.
+     */
+    public NotificationMessageBuilderHelper(final String inBaseUrl)
+    {
+        baseUrl = inBaseUrl;
+    }
+
     /**
      * Returns a variable-substituted version of the activity's body.
-     * 
+     *
      * @param activity
      *            Activity.
      * @param context
@@ -59,5 +84,75 @@ public class NotificationMessageBuilderHelper
         }, VARIABLE_START_MARKER, VARIABLE_END_MARKER, StrSubstitutor.DEFAULT_ESCAPE);
         String result = transform.replace(activity.getBaseObjectProperties().get("content"));
         return result;
+    }
+
+    /**
+     * Resolves the subset of markdown supported by Eureka into appropriate text.
+     *
+     * @param input
+     *            String potentially with markdown.
+     * @return String with markdown replaced.
+     */
+    public String resolveMarkdownForText(final String input)
+    {
+        Matcher matcher = MARKDOWN_LINK_PATTERN.matcher(input);
+        StringBuffer sb = new StringBuffer();
+        while (matcher.find())
+        {
+            matcher.appendReplacement(sb, ""); // use empty string here since it treats $ and \ specially
+            String text = matcher.group(1);
+            String url = matcher.group(2);
+            if (url.charAt(0) == '#')
+            {
+                url = baseUrl + url;
+            }
+            sb.append(text).append(" (").append(url).append(")");
+        }
+        if (sb.length() > 0)
+        {
+            matcher.appendTail(sb);
+            return sb.toString();
+        }
+        else
+        {
+            return input;
+        }
+    }
+
+    /**
+     * Resolves the subset of markdown supported by Eureka into appropriate text and HTML-escapes all input.
+     *
+     * @param input
+     *            String potentially with markdown.
+     * @return String with markdown replaced.
+     */
+    public String resolveMarkdownForHtml(final String input)
+    {
+        Matcher matcher = MARKDOWN_LINK_PATTERN.matcher(input);
+        StringBuffer sb = new StringBuffer();
+        int copyFrom = 0;
+        while (matcher.find())
+        {
+            sb.append(StringEscapeUtils.escapeHtml(input.substring(copyFrom, matcher.start())));
+            copyFrom = matcher.end();
+
+            String text = matcher.group(1);
+            String url = matcher.group(2);
+            if (url.charAt(0) == '#')
+            {
+                url = baseUrl + url;
+            }
+            sb.append("<a href=\"").append(StringEscapeUtils.escapeHtml(url)).append("\">")
+                    .append(StringEscapeUtils.escapeHtml(text)).append("</a>");
+        }
+        if (copyFrom > 0)
+        {
+            sb.append(StringEscapeUtils.escapeHtml(input.substring(copyFrom, input.length())));
+            return sb.toString();
+        }
+        else
+        {
+            return StringEscapeUtils.escapeHtml(input);
+        }
     }
 }
