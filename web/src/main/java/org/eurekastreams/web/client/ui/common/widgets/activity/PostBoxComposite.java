985c63a07aa552ebfdfa87833634df58b5a75fe4
Rob Keane <robert.c.keane@lmco.com>
2011-07-20 12:02:34 -0400

    Checkstyle fix

--- web/src/main/java/org/eurekastreams/web/client/ui/common/widgets/activity/PostBoxComposite.java
+++ web/src/main/java/org/eurekastreams/web/client/ui/common/widgets/activity/PostBoxComposite.java
@@ -61,7 +61,6 @@ import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTMLPanel;
