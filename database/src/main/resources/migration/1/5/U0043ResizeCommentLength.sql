216902f6dd6b35827aea8b358a97f0c32cb76992
Rob Keane <robert.c.keane@lmco.com>
2011-07-27 09:57:43 -0400

    Fix for typo

--- database/src/main/resources/migration/1/5/U0043ResizeCommentLength.sql
+++ database/src/main/resources/migration/1/5/U0043ResizeCommentLength.sql
@@ -1,3 +1,3 @@
 insert into db_version (major, minor, patch, scriptname, description) values (1, 5, '0043', 'U0043ResizeCommentLength', 'Resize body column in comment table.');
 
-ALTER TABLE comments ALTER COLUMN body TYPE varchar(1000);
+ALTER TABLE comment ALTER COLUMN body TYPE varchar(1000);
