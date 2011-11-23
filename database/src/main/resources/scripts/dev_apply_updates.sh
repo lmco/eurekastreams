cf34212d4dbf005122cfd32853fbf18414fffdab
billwolckenlmco <bill.wolcken@lmco.com>
2011-11-23 18:13:16 -0500

    Updated dev_apply_updates to work on both Linux (Red Hat) and Mac OSX.  Also fixed a couple issues.

--- database/src/main/resources/scripts/dev_apply_updates.sh
+++ database/src/main/resources/scripts/dev_apply_updates.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 # apply_updates.sh:  Applies database update scripts where necessary
+#
+# In order to run successfully when the database doesn't already exist, insure one of the following is the case:
+# - The desired dbuser role already exists
+# - pg_hba.conf allows "trust" mode for the database being created or the dbuser role.  (Ideally specify both in the
+#   rule to limit the amount of trust going on.)
+# This is because the script (in its current form, which should be changed) creates the role without setting a password
+# then connects as that role to run the migrations.  This will fail if any mode other than "trust" is in play because:
+# - if it used password/md5/etc.:  because postgres will forbid a login when the password isn't set
+# - if it used ident/etc.:  The prior connections (for checking the db, creating the user, etc.) don't specify a user,
+#   so they use the current OS user.  The current OS user cannot be both an existing superuser and a new user, so if the
+#   prior calls as the OS user worked, then the ones as the new user will not match up with the OS user and will fail.
+#
 if [[ -n %1 ]]
 then
         force=%1
@@ -12,11 +24,29 @@ xmlfile="/etc/eureka/eureka.xml"
 newline="
 "
 
-# This script requires xmlstarlet to be installed.  Check to see which executable name is being used by xmlstarlet.
-if command -v xml &>/dev/null; then xmlcommand=xml; else xmlcommand=xmlstarlet; fi
 
 # Check the execution path.  The script must be run from the scripts directory.
 exepath=`pwd`
+
+
+function PrepareDirectory 
+{
+	if [ -d $1 ]; then
+		if [ ! -w $1 ]; then
+			echo "$2 folder $1 is not writable." >&2
+			exit 1
+		fi
+	elif [ -e $1 ]; then
+		echo "$2 path $1 exists but is not a folder." >&2
+		exit 1
+	else
+		mkdir -p $1
+		if [ $? -ne 0 ]; then
+			echo "Cannot create $2 folder $1." >&2
+			exit 1
+		fi
+	fi
+}
 
 function WriteLog {
         if [[ ! -e $logdir ]]
@@ -24,21 +54,62 @@ function WriteLog {
                 mkdir -p $logdir
         fi
         echo `date +%Y-%m-%d-%T` $* >> $fulllogfile
-        if [[ $* =~ "^\[DEBUG" ]]
-        then
-                if [[ $debugmode == 1 ]]
-                then
-                        echo $@
-                fi
-        else
-                echo $@
+        if [[ $* != \[DEBUG\]* || $debugmode -eq 1 ]]; then
+    		echo $@
         fi
 }
+
+function SedInPlace {
+	case "$sedtype" in
+		gnu) sed --in-place "$1" $2 ;;
+		bsd) sed -i "" "$1" $2 ;;
+	esac
+}
+
+# TEMPORARY HACK way of determining temp dirs
+# This should use switches and wrapper scripts, not a self-aware script that must 
+# have a certain name to get the desired behavior.
+scriptname=`basename $0`
+if [[ "$scriptname" == 'dev_apply_updates.sh' ]]
+then 
+	tempdir=$HOME/tmp
+	logdir=$HOME/logs
+else 
+	tempdir=/tmp
+	logdir=/var/log/eureka-database
+fi
+
+# prepare temp use dirs
+PrepareDirectory $logdir "Log"
+PrepareDirectory $tempdir "Temporary"
+
+# determine sed in-place option format to use
+sedtest=$tempdir/sedtest.tmp
+touch $sedtest
+if [[ ! -f $sedtest || ! -w $sedtest ]]; then
+	echo "Cannot create temp file $sedtest." >&2
+	exit 1
+fi
+sed --in-place Q $sedtest 2>/dev/null
+if [ $? -eq 0 ]; then
+	sedopt='--in-place'
+	sedtype="gnu"
+else
+	sed -i "" q $sedtest 2>/dev/null
+	if [ $? -eq 0 ]; then
+		sedopt='-i ""'
+		sedtype="bsd"
+	else
+		rm $sedtest
+		echo "Cannot determine proper sed invocation." >&2
+		exit 1
+	fi
+fi
+rm $sedtest
 
 
 # Check to see which name this script ran as.  If it's dev_apply_updates.sh, then this was run by a developer.
 scriptname=`basename $0`
-
 if [[ "$scriptname" == 'dev_apply_updates.sh' ]]
 then
         asdev=1;
@@ -46,9 +117,8 @@ then
         dbname=$tempdbname
         dbuser=$tempdbuser
         appurl=$tempdevhost
-        logdir=$HOME/logs
         fulllogfile=$logdir/$logfilebase`date +%Y%m%d-%H%M`.log
-        if [[ ! "$exepath" =~ 'target' ]]
+        if [[ $exepath != */target/* ]]
         then
                 # If running as a developer, the path must contain "target"
                 WriteLog "[ERROR] You can only run database change scripts from within a deployed target."
@@ -64,8 +134,18 @@ else
         logfilebase=eurekastreams
         dbname=eurekastreams
         dbuser=eurekastreams
-        logdir=/var/log/eureka-database
         fulllogfile=$logdir/$logfilebase`date +%Y%m%d-%H%M`.log
+        
+        # This script requires xmlstarlet to be installed.  Check to see which executable name is being used by xmlstarlet.
+		xmlcommand=`command -v xml`
+		if [ $? -ne 0 ]; then
+			xmlcommand=`command -v xmlstarlet`
+			if [ $? -ne 0 ]; then
+				echo "Cannot locate xmlstarlet" >&2
+				exit 1
+			fi
+		fi
+        
         appurl=`$xmlcommand sel -t -v "//url" $xmlfile`
         WriteLog "[DEBUG] App URL: $appurl"
         dbhost=`$xmlcommand sel -t -v "//node[capabilities/capability='Database Server']/network/hostname" $xmlfile`
@@ -74,7 +154,7 @@ else
 fi
 
 
-if [[ ! $exepath =~ .*scripts$ ]]
+if [[ $exepath != *scripts ]]
 then
         WriteLog "[ERROR] This script must be executed from within the directory where the scripts reside."
         exit 1
@@ -86,10 +166,11 @@ function ApplyLoop ()
 
         # Iterate through all major versions from the current to the new
         WriteLog "[DEBUG] Iterating from major version $currmajor to $newmajor"
-        for majorver in `seq $currmajor $newmajor`
+        majorver=$currmajor
+        while [ $majorver -le $newmajor ]
         do
                 WriteLog "[DEBUG] Iterating through major version $majorver"
-                if [[ $majorver == $currmajor ]]
+                if [[ $majorver -eq $currmajor ]]
                 then
                         minorstarter=$currminor
                 else
@@ -98,40 +179,49 @@ function ApplyLoop ()
                 maxminorinmajor=`ls -1 $exepath/../migration/$majorver|tail -1`
                 WriteLog "[DEBUG] Iterating from minor version $minorstarter to $maxminorinmajor"
                 # Iterate through all minor versions from the current to the new
-                for minorver in `seq $minorstarter $maxminorinmajor`
+                minorver=$minorstarter
+                while [ $minorver -le $maxminorinmajor ]
                 do
-                        if [[ $minorver == $currminor ]]
+                        if [[ $minorver -eq $currminor ]]
                         then
-                                patchstarter=$((`echo $currpatch|sed 's/^0\+//g'`+1))
+                                patchstarter=$(( 10#$currpatch + 1 ))
                         else
                                 patchstarter=0
                         fi
                         maxpatchfile=`ls -1 $exepath/../migration/$majorver/$minorver|tail -1`
-                        maxpatchinminor=$((`echo ${maxpatchfile:1:4}|sed 's/^0\+//g'`))
+						maxpatchinminor=$(( 10#${maxpatchfile:1:4} ))
 
                         WriteLog "[DEBUG] Iterating in version $majorver.$minorver from patch $patchstarter to $maxpatchinminor"
                         # Iterate through all patches from the current (if applicable) to the new.
-                        for patchver in `seq $patchstarter $maxpatchinminor`
+                        patchver=$patchstarter
+                        while [ $patchver -le $maxpatchinminor ]
                         do
                                 formattedpatchver=`printf "%04d" $patchver`
                                 for script in $exepath/../migration/$majorver/$minorver/U$formattedpatchver*.sql
                                 do
+                                	if [ ! -f $script ]; then
+                                		WriteLog "[WARN] Missing script $script."
+                                	else
                                         WriteLog "[INFO] Concatenating $script to the full execution script."
-                                        WriteLog `echo "$newline$newline-- From $script$newline" >> $fullscript`
-                                        WriteLog `cat $script>>$fullscript`
-                                        exitcode=`echo $?`
-                                        if [[ $exitcode == 0 ]]
-                                        then
-                                                returnval=0
-                                        else
-                                                WriteLog "[ERROR] Concatenation of $script failed with a bash exit code of "$exitcode
-                                                WriteLog "[ERROR] A problem has occurred concatenating $script.  No database changes have occurred.  Check the log for details."
-                                                returnval=$exitcode
-                                                break
+                                        cmdoutput=`echo "$newline$newline-- From $script$newline" >> $fullscript 2>&1`
+                                        rc=$?
+                                        if [ $rc -eq 0 ]; then
+                                        	cmdoutput=`cat $script>>$fullscript 2>&1`
+                                        	rc=$?
                                         fi
+                                        if [ $rc -ne 0 ]; then
+                                        	[ -n "$cmdoutput" ] || WriteLog "$cmdoutput"
+                                        	WriteLog "[ERROR] Concatenation of $script failed with a exit code of $rc"
+                                        	WriteLog "[ERROR] A problem has occurred concatenating $script.  No database changes have occurred.  Check the log for details."
+                                        	return $rc
+                                        fi
+                                    fi
                                 done
+                                let patchver=patchver+1
                         done
+                        let minorver=minorver+1
                 done
+                let majorver=majorver+1
         done
         WriteLog "[INFO] All applicable scripts have been concatenated."
         return $returnval
@@ -140,26 +230,33 @@ function ApplyLoop ()
 function ApplyBase ()
 {
         # Apply schema script 1, then cat the rest for running in a transaction
-        for dbscript in `ls -1 $exepath/../schema/*.sql -1`
+        for dbscript in `ls -1 $exepath/../schema/*.sql`
         do
-                if [[ $dbscript =~ '_1_' ]]
+                if [[ $dbscript == *_1_* ]]
                 then
                         WriteLog "[INFO] Replacing tokens in $dbscript..."
                         # Change database user
-                        sed "s/OWNER TO eurekastreams/OWNER TO $dbuser/g" $dbscript >> $fullscript\_create.sql
+                        sed "s/OWNER TO eurekastreams/OWNER TO $dbuser/g" $dbscript >> ${fullscript}_create.sql
+                        SedInPlace "s/Owner: eurekastreams/Owner: $dbuser/g" ${fullscript}_create.sql
                         # Change database name
-                        sed -i "s/eurekastreams/$dbname/g" $fullscript\_create.sql
+						# Note:  Be VERY specific about what gets matched.  This does mean that changing the _1_ script
+						# may require updates here.  But it needs to insure that it ONLY matches where a database name
+						# is intended, otherwise if the user chooses a dbuser of "eurekastreams" it will match the
+						# places that have already been replaced with dbuser.  
+                        SedInPlace "s/DATABASE eurekastreams/DATABASE $dbname/g" ${fullscript}_create.sql
+                        SedInPlace "s/EXISTS eurekastreams/EXISTS $dbname/g" ${fullscript}_create.sql
+                        SedInPlace "s/\c eurekastreams/\c $dbname/g" ${fullscript}_create.sql
                         # Change the application URL
-                        sed -i "s,http://localhost:8080,$appurl,g" $fullscript\_create.sql
+                        SedInPlace "s,http://localhost:8080,$appurl,g" ${fullscript}_create.sql
                         WriteLog "[INFO] Executing $dbscript to initialize the database."
-                        mycommand=`psql -h $dbhost -d postgres -f $fullscript\_create.sql 2>&1 1>/dev/null`
+                        mycommand=`psql -h $dbhost -d postgres -f ${fullscript}_create.sql 2>&1 1>/dev/null`
                         WriteLog "[DEBUG] Output from execution: $mycommand"
                         if [[ ! `echo $?` == 0 ]]
                         then
                                 WriteLog "[ERROR] $dbscript had a problem executing.  Exit code is `echo $?`."
                                 return 1
                         fi
-                        if [[ $mycommand =~ "FATAL" ]]
+                        if [[ $mycommand == *FATAL* ]]
                         then
                                 WriteLog "[ERROR] A problem occurred.  See $fulllogfile for details."
                                 exit 1
@@ -201,7 +298,16 @@ function main ()
                         exit 1
                 else
                         WriteLog "[INFO] No database found.  Creating the $dbuser user."
-                        createuser -S -D -R $dbuser
+                        cmdoutput=`createuser -S -D -R -h $dbhost $dbuser 2>&1`
+                        if [ $? -ne 0 ]; then
+                        	if [[ $cmdoutput == *already\ exists ]]; then
+                        		WriteLog "[INFO] User $dbuser already exists."
+                        	else
+                        		WriteLog "[ERROR] Failed to create user $dbuser.  $cmdoutput"
+                        		exit 1
+                        	fi
+                        fi
+                        
                         # Initialize version variables to the lowest possible in the system
                         currmajor=0
                         currminor=7
@@ -209,7 +315,7 @@ function main ()
                 fi
         fi
 
-# Get the deployment's database version
+		# Get the deployment's database version
         newmajor=`ls -1 $exepath/../migration|tail -1`
         newminor=`ls -1 $exepath/../migration/$newmajor|tail -1`
         newpatch=`ls -1 $exepath/../migration/$newmajor/$newminor|tail -1`
@@ -238,11 +344,11 @@ function main ()
                 #Todo:  Implement sed commands
                 WriteLog "[DEBUG] Replacing tokens in the database scripts..."
                 # Change database user
-                sed -i "s/OWNER TO eurekastreams/OWNER TO $dbuser/g" $fullscript
-                # Change database name
-                sed -i "s/ eurekastreams/ $dbname/g" $fullscript
+                SedInPlace "s/OWNER TO eurekastreams/OWNER TO $dbuser/g" $fullscript
+                SedInPlace "s/Owner: eurekastreams/Owner: $dbuser/g" $fullscript
+                # Note:  Not checking for the database name since the scripts do not and should not reference it.
                 # Change the application URL
-                sed -i "s,http://localhost:8080,$appurl,g" $fullscript
+                SedInPlace "s,http://localhost:8080,$appurl,g" $fullscript
 
         # Run the $fullscript
                 WriteLog "[INFO] Modifying the database..."
@@ -250,7 +356,7 @@ function main ()
                 # Note:  connect to the postgres database, then let the scripts do the connect to the $dbname database
                 mycommand=`psql -U $dbuser -h $dbhost -d $dbname -t -f $fullscript -1 2>&1`
                 WriteLog "[DEBUG] Output from execution: $mycommand"
-                if [[ $mycommand =~ 'commands ignored until end of transaction block$' ]] || [[ $mycommand =~ "FATAL" ]] || [[ $mycommand =~ "ERROR" ]]
+                if [[ $mycommand == *commands\ ignored\ until\ end\ of\ transaction\ block ]] || [[ $mycommand == *FATAL* ]] || [[ $mycommand == *ERROR* ]]
                 then
                         WriteLog "[ERROR] A problem occurred with the application of the latest scripts.  Your database has not been updated. See the log file for details: $fulllogfile"
                         if [[ -n $newdb ]]
