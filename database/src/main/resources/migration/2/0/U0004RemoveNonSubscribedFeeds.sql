8aa77ba1bfa6dd34a16467ad0904a095db122e1a
Blake Caldwell <w.blake.caldwell@lmco.com>
2012-01-04 17:14:28 -0500

    SQL script to delete all Feeds with no subscribers

---
+++ database/src/main/resources/migration/2/0/U0004RemoveNonSubscribedFeeds.sql
@@ -0,0 +1,7 @@
+insert into db_version (major, minor, patch, scriptname, description) values (2, 0, '0004', 'U0004RemoveNonSubscribedFeeds', 'Deletes all Feed entries that do not have subscribers.');
+
+-- Delete all feeds that have no subscribers
+DELETE FROM Feed WHERE id IN 
+(
+	SELECT f.id FROM Feed f LEFT OUTER JOIN FeedSubscriber fs ON f.id = fs.feedId GROUP BY f.id HAVING COUNT(fs.feedId)=0
+)
