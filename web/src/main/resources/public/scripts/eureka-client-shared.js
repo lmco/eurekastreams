57fddb8e96b50531aaaaadf5a54204bd75af27d4
billwolckenlmco <bill.wolcken@lmco.com>
2012-02-02 18:16:08 -0500

    Additional updates with content parsing.

--- web/src/main/resources/public/scripts/eureka-client-shared.js
+++ web/src/main/resources/public/scripts/eureka-client-shared.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2012 Lockheed Martin Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Note:  This SHOULD be minified and concatenated on to external-js-min.js during the build.  (Also would need to
+// remove the script tag for it in index.html and widget.html to not load it twice.)
+
 (function() {
 	// Create the namespace
 	if(typeof EurekaStreams === 'undefined'){
