eaeb87809a5f389b02827d6fc0dfbfbc68eacaf6
kmmeureka <kevin.m.meredith@lmco.com>
2012-12-07 16:57:59 -0500

    removed badly named files

--- database/src/main/resources/migration/2/0/U008RemoveBookmarksToDeletedGroups.sql
+++
@@ -1,4 +0,0 @@
-insert into db_version (major, minor, patch, scriptname, description) values (2, 0, '0008', 'U008RemoveBookmarksToDeletedGroups.sql', 'Remove bookmarks to deleted groups.');
-
-delete from person_bookmark pb where not exists (select 1 from streamscope b where b.id = pb.scopeid);
-
