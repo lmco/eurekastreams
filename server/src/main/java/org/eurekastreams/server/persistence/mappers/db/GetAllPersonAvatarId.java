cce1b3cc917fe2a52383b1b7b9f3dd042f432720
George <george.apsokardu@lmco.com>
2013-10-03 12:16:13 -0400

    adding in a new endpoint to expose avatars and adding in CORs support with restrictions

---
+++ server/src/main/java/org/eurekastreams/server/persistence/mappers/db/GetAllPersonAvatarId.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2013 Lockheed Martin Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eurekastreams.server.persistence.mappers.db;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.eurekastreams.server.persistence.mappers.ReadMapper;
+
+import edu.emory.mathcs.backport.java.util.Arrays;
+
+/**
+ * This mapper just retrieves all People with accountId and additionalProperties populated from the db.
+ * 
+ */
+public class GetAllPersonAvatarId extends ReadMapper<String, List<Map<String, Object>>>
+{
+	private final Log logger = LogFactory.getLog(GetAllPersonAvatarId.class);
+    /**
+     * Return a single Person object in the db with only accountId, email and additionalProperties populated. 
+     * {@inheritDoc}
+     * .
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Map<String, Object>> execute(final String inRequest)
+    {
+        return (List<Map<String, Object>>) getEntityManager().createQuery(
+                "select new map(i.imageIdentifier as imageIdentifier, i.imageBlob as imageBlob)"
+                        + " from Image i where i.imageIdentifier in (:avatarids)")
+                        .setParameter("avatarids", new ArrayList<String>(Arrays.asList(inRequest.split(",")))).getResultList();
+    }
+}
